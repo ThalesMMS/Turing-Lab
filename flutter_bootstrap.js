2fef3cef1c50b832c0816d2980cf111192028eb4
ThalesMMS <tmmscomp@gmail.com>
2026-09-02 00:48:39 -0300

    web: publish build from main 87408641

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "567761389" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1491958278" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
